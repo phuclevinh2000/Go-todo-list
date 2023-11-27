0a5c1d1f4a2d464707db9eec335e29d6b3e60ac0
Phuc Le <phuclevinh2000@gmail.com>
2023-11-27 21:27:02 +0200

    create base fe and get and patch method for be

---
+++ server/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
+func main() {
+	fmt.Print("Hello world")
+
+	app := fiber.New()
+
+	todos := []Todo{}
+
+	app.Get("/healthcheck", func(ctx *fiber.Ctx) error {
+		return ctx.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(ctx *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := ctx.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return ctx.JSON(todos)
+
+	})
+
+	app.Patch("/api/todos/:id/done", func(ctx *fiber.Ctx) error {
+		id, err := ctx.ParamsInt("id")
+
+		if err != nil {
+			return ctx.Status(401).SendString("Invalid id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return ctx.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
